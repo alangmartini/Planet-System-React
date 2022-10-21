380260ba0382270b844af76d5c9f01431780181b
gmaralan <gmartinialan@gmail.com>
2022-10-21 14:05:30 -0400

    req 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 export default class Missions extends Component {
   render() {
+    function renderMissions(arrOfMissions) {
+      return arrOfMissions
+        .map(({ name, year, country, destination }, index) => (
+          <MissionCard
+            key={ index }
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+          />
+        ));
+    }
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        { renderMissions(missions) }
       </div>
     );
   }
